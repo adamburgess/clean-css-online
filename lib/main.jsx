97c7e9823f7cd968a46028e3646f7ddb63656599
AdamBurgess <AdamBurgess@users.noreply.github.com>
2015-03-08 06:13:02 +1100

    refactor + add warnings

--- lib/main.jsx
+++ lib/main.jsx
@@ -9,8 +9,27 @@ var highlight = hljs.highlight;
 import './main.css!'
 import mainCss from './main.css!text'
 
-var ww = new Worker('worker.js');
+var worker = new Worker('worker.js');
 
+var Stats = React.createClass({
+  render: function() {
+    var stats = this.props.stats;
+    var generatedStr = stats.timeSpent < 2 ? ' Generated instantly.' : ` Generated in ${stats.timeSpent / 1000} seconds.`;
+    return <div className="stats">
+      Source: {stats.originalSize} characters.
+      Minified: {stats.minifiedSize} characters.
+      Saved {Math.round(stats.efficiency * 10000) / 100}%.
+      {generatedStr}
+    </div>;
+  }
+});
+
+var Warnings = React.createClass({
+  render: function() {
+    var ws = this.props.warnings.map(w => <div key={w} className="warning">Warning: {w}</div>);
+    return this.props.warnings.length > 0 ? <div className="warnings">{ws}</div> : <span />;
+  } 
+})
 var Container = React.createClass({
     getInitialState: function() {
         return {
@@ -21,23 +40,17 @@ var Container = React.createClass({
     },
     render: function() {
         var html = highlight('css', this.state.minified).value;
-        var stats = this.state.stats;
-        var generatedStr = stats.timeSpent < 2 ? ' Generated instantly.' : ` Generated in ${stats.timeSpent / 1000} seconds.`;
         return <div>
             <textarea className="input io" defaultValue={mainCss} onChange={this.minify} />
             <div className="output io">
+              <Stats stats={this.state.stats} />
+              <Warnings warnings={this.state.warnings} />
               <div className="hljs" dangerouslySetInnerHTML={{__html: html}} />
-              <div className="stats">
-                Source: {stats.originalSize} characters.
-                Minified: {stats.minifiedSize} characters.
-                Saved {Math.round(stats.efficiency * 10000) / 100}%.
-                {generatedStr}
-              </div>
             </div>
         </div>;
     },
     componentDidMount: function() {
-        ww.onmessage = e => {
+        worker.onmessage = e => {
             var data = e.data;
             this.setState({ minified: data.styles, warnings: data.warnings, stats: data.stats });
         }
@@ -51,7 +64,7 @@ var Container = React.createClass({
         var text = e.target.value;
         if(text.length > 50000) {
             //use ww
-            ww.postMessage(text);
+            worker.postMessage(text);
         } else {
             var data = minify(text);
             console.log(data);
